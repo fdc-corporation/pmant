397f22cc0cd1af3acff8becb9744ba06925d4f6b
palaciosyost <palaciosyostin903@gmail.com>
2025-05-15 14:27:44 -0500

    update js data.js

--- static/src/js/data.js
+++ static/src/js/data.js
@@ -1,43 +1,49 @@
 /** @odoo-module **/
 
+import { whenReady } from "@web/core/utils/browser";
 
-document
-  .getElementById("enviar_medicion")
-  .addEventListener("click", async function () {
-    const data = {
-      equipo_id: document.getElementById("equipo_id").value,
-      uso_promedio: document.getElementById("uso_promedio").value,
-      temperatura: document.getElementById("temperatura").value,
-      presion: document.getElementById("presion").value,
-      tem_ambiente: document.getElementById("tem_ambiente").value,
-    };
-    console.log(data)
-    try {
-      const response = await fetch("/guardar/medicion/equipo", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
+whenReady(() => {
+  const boton = document.getElementById("enviar_medicion");
+  if (boton) {
+    boton.addEventListener("click", async function () {
+      const data = {
+        equipo_id: document.getElementById("equipo_id").value,
+        uso_promedio: document.getElementById("uso_promedio").value,
+        temperatura: document.getElementById("temperatura").value,
+        presion: document.getElementById("presion").value,
+        tem_ambiente: document.getElementById("tem_ambiente").value,
+      };
+      console.log(data);
+      try {
+        const response = await fetch("/guardar/medicion/equipo", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        });
 
-      const result = await response.json();
-      console.log(result)
-      if (result.result.success) {
-        mostrarNotificacion("notificacion-exito");
-      } else {
+        const result = await response.json();
+        console.log(result);
+        if (result.result.success) {
+          mostrarNotificacion("notificacion-exito");
+        } else {
+          mostrarNotificacion("notificacion-error");
+        }
+      } catch (error) {
+        console.error("Error al enviar datos:", error);
         mostrarNotificacion("notificacion-error");
       }
-    } catch (error) {
-      console.error("Error al enviar datos:", error);
-      mostrarNotificacion("notificacion-error");
-    }
-  });
+    });
+  }
 
-function mostrarNotificacion(id) {
-  const noti = document.getElementById(id);
-  noti.style.display = "block";
-  setTimeout(() => {
-    noti.style.display = "none";
-  }, 4000);
-}
+  function mostrarNotificacion(id) {
+    const noti = document.getElementById(id);
+    if (noti) {
+      noti.style.display = "block";
+      setTimeout(() => {
+        noti.style.display = "none";
+      }, 4000);
+    }
+  }
+});
