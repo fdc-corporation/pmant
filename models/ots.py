7811ce768a8ac26fc89b36bbe5e77abe88054e29
palaciosyost <palaciosyostin903@gmail.com>
2025-05-26 10:51:57 -0500

    Update de funcionalidades y view de tarea.mantenimiento

--- models/ots.py
+++ models/ots.py
@@ -529,7 +529,7 @@ class OTS(models.Model):
 
     def _get_cantidad_incidencias(self):
         for record in self:
-            cant_data = self.env["inconveniente.servicio"].search([("ot_id", "=", record.id)])
+            cant_data = self.env["inconveniente.servicio"].search([("ot_id", "in", record.id)])
             record.cantidad_inconvenientes = len(cant_data)
 
     def action_view_incidencias(self):
