fd631f085c6fec4babae226c2f776dbf950550f7
palaciosyost <palaciosyostin903@gmail.com>
2025-08-11 18:26:10 -0500

    cambios de icono web

--- models/tarea.py
+++ models/tarea.py
@@ -93,6 +93,27 @@ class Tarea(models.Model):
         help='Color de la tarea, utilizado en el kanban y en la vista de lista.'
     )
     sale_order = fields.Many2one("sale.order", string="Orden de venta")
+    cotizacion_cantidad = fields.Integer(compute="_total_cotizaciones")
+    compania = fields.Many2one(
+        comodel_name="res.company",
+        string="Compañía",
+        default=lambda self: self.env.company,
+        required=True,
+    )
+
+    def _total_cotizaciones(self):
+        self.cotizacion_cantidad = len(self.cotizacion_id)
+
+    def action_view_cotizaciones(self):
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Ventas",
+            "view_mode": "form",
+            "res_model": "sale.order",
+            "res_id": self.sale_order.id,
+            "context": "{'create' : False}",
+        }
+
 
     @api.model
     def _group_expand_stages(self, stages, domain, order):
