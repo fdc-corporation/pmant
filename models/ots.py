8a375e649682f11855bbb59ae5c15dd172c26304
palaciosyost <palaciosyostin903@gmail.com>
2025-11-12 12:24:16 -0500

    update fecha ejecutada

--- models/ots.py
+++ models/ots.py
@@ -316,13 +316,13 @@ class MaintenanceRequestOTS(models.Model):
                 # Renderizar asunto y cuerpo para el chatter
                 subject = template._render_field("subject", [rec.id])[rec.id]
                 body_html = template._render_field("body_html", [rec.id])[rec.id]
-
-                rec.message_post(
-                    body=body_html or _("✅ Correo de programación enviado."),
-                    subject=subject or _("Correo de programación"),
-                    message_type="comment",
-                    subtype_xmlid="mail.mt_note",
-                )
+                mail_id.action_send_and_close()
+                # rec.message_post(
+                #     body=body_html or _("✅ Correo de programación enviado."),
+                #     subject=subject or _("Correo de programación"),
+                #     message_type="comment",
+                #     subtype_xmlid="mail.mt_note",
+                # )
 
                 _logger.info("Correo de programación enviado para OT %s (mail_id=%s)", rec.id, mail_id)
 
